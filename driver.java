ed9c268f95b772830f4cc9424facba210b7a54b4
James-Chun <jchun00@stuy.edu>
2018-11-06 00:22:25 -0500

    Driver

--- driver.java
+++ driver.java
@@ -1,101 +1,164 @@
-public class driver{
-	public static void main(String[] args){
-		SuperArray a = new SuperArray();
-		/*Phase 1 testing*/
-		System.out.println("-----Testing Phase 1-----");
+public class Driver {
 
-		//Testing SuperArray initializing
-		System.out.println("\n---Initializing SuperArray---");
-		System.out.println("Testing toString(): should return []");
-		System.out.println(a);
-		System.out.println("\nTesting toStringDebug(): should return [null (x10)]");
-		System.out.println(a.toStringDebug());
+  public static void main(String[] args) {
 
-		//testing size()
-		System.out.println("\n\n---Testing size()---");
-		System.out.println("Testing size(): should return 0");
-		System.out.println(a.size());
 
-		//testing add()
-		System.out.println("\n\n---Testing add()---");
-		System.out.println("Testing add() while empty: should return true");
-		System.out.println(a.add("apple"));
-		System.out.println("\nChecking add() and toString(): should return [apple]");
-		System.out.println(a);
-		a.add("banana");
-		System.out.println("\nChecking add() and toString(): should return [apple, banana]");
-		System.out.println(a);
-		for (int x = 0; x < 9; x++){
-			a.add("blank");
-		}
-		System.out.println("\n---Testing toStringDebug() again: should print [apple, banana, blank (x8)]---");
-		System.out.println(a.toStringDebug());
-		System.out.println("\n\n---Testing add() while full: should return false---");
-		System.out.println(a.add("full"));
+    System.out.println("TESTING PHASE 1 and 2... except clear() which will be at the end");
 
-		//testing clear()
-		System.out.println("\n\n---Testing clear()---");
-		System.out.println("Testing clear(): should return []");
-		a.clear();
-		System.out.println(a);
-		System.out.println("\n\n---Testing toStringDebug(): Should print [null (x10)]---");
-		System.out.println(a.toStringDebug());
-		System.out.println("\n---Testing clear() and size(): should return 0---");
-		System.out.println(a.size());
-		System.out.println("\n---Testing add(\"boop\") after clear(): should return true---");
-		System.out.println(a.add("boop"));
-		System.out.println("\n---Should print [boop]---");
-		System.out.println(a);
-		a.clear();
+    System.out.println();
+    System.out.println();
+    System.out.println("Testing SuperArray(int) ... SA should be [] even when debugged");
+    SuperArray SA = new SuperArray(0);
 
-		//testing get()
-		System.out.println("\n\n-----Testing get()-----");
-		System.out.println("---Testing get() while empty: should return null---");
-		System.out.println(a.get(0));
-		a.add("burp");
-		a.add("ahem");
-		a.add("coughdrop");
-		System.out.println("\n---Testing get() with indices 0-2 filled: should return values burp, ahem, coughdrop---");
-		System.out.println(a.get(0));
-		System.out.println(a.get(1));
-		System.out.println(a.get(2));
-		System.out.println("\n---Testing get() for out of bounds: should return null---");
-		System.out.println(a.get(-1));
-		System.out.println(a.get(10));
+    System.out.println("Testing toString() and toStringDebug ... Expected: []");
+    System.out.println("Current Status of SA: " + SA.toStringDebug());
 
-		//testing set()
-		System.out.println("\n\n-----Testing set()-----");
-		System.out.println("---Using SuperArray from testing get()---");
-		System.out.println(a);
-		System.out.println("\n---Testing set(0, \"blue\"): should return burp---");
-		System.out.println(a.set(0, "blue"));
-		System.out.println("-Should print [blue, ahem, coughdrop]-");
-		System.out.println(a);
-		System.out.println("\n---Testing set(1, \"fish\"): should return ahem---");
-		System.out.println(a.set(1, "fish"));
-		System.out.println("-Should print [blue, fish, coughdrop]-");
-		System.out.println(a);
-		System.out.println("\n---Testing set(2, \"syrup\"): should return coughdrop---");
-		System.out.println(a.set(2, "syrup"));
-		System.out.println("-Should print [blue, fish, syrup]-");
-		System.out.println(a);
-		System.out.println("\n\n---Testing the error: using indices that are outside of the domain. Both should return null and print an error message---");
-		System.out.println(a.set(-2, "I'm not to be seen"));
-		System.out.println(a.set(100, "I'm not supposed to be here..."));
-		System.out.println("\n---Using cleared SuperArray---");
-		a.clear();
-		System.out.println("\n---Testing set(2, \"rock\"): should return null and an error message---");
-		System.out.println(a.set(2, "rock"));
-		System.out.println("-Should print []-");
-		System.out.println(a);
+    System.out.println();
 
-		//Testing contains
-		a.add("butt");
-		a.add("stuff");
-		a.add("woo");
-		System.out.println ("---should return true---");
-		System.out.println (a.contains("woo"));
-		System.out.println ("---should return false---");
-		System.out.println (a.contains("hi"));
-	}
+    System.out.println("Testing isEmpty() ... Expected : true");
+    System.out.println("isEmpty?: " + SA.isEmpty());
+
+    System.out.println();
+
+    System.out.println("Testing add(String) ... Expected : true");
+    System.out.println(SA.add("A"));
+    System.out.println("Expected : [A]");
+    System.out.println("Testing toString() ... Current Status of SA : " + SA);
+    System.out.println("Testing isEmpty() ... Expected : false");
+    System.out.println("isEmpty?: " + SA.isEmpty());
+    System.out.println("Testing size() ... Expected : 1");
+    System.out.println("Size: " + SA.size());
+
+    System.out.println();
+
+    System.out.println("Adding \"B\" to SA?: " + SA.add("B"));
+    System.out.println("Testing resize() and toStringDebug ... Expected [A, B, null]");
+    System.out.println("Current Status of SA: " + SA.toStringDebug());
+    System.out.println("Testing get(int) ... Expected : B");
+    System.out.println("SA[1] == " + SA.get(1));
+    System.out.println("Testing get(int)\'s throw ... Expected : 2 error messages");
+    try{
+      SA.get(2);
+    } catch(IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in get()");
+    }
+    try{
+      SA.get(-2);
+    } catch (IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in get()");
+    }
+
+    System.out.println();
+/*
+    System.out.println("Testing set(int,String) ...  Expected : A");
+    System.out.println("\"C\" will replace : " + SA.set(0,"C"));
+    System.out.println("Expected : [C, B]");
+    System.out.println("SA\'s Current Status: " + SA);
+    System.out.println("Testing set(int,String)\'s throw ... Expected : 2 error messages");
+    try{
+      SA.set(2,"C");
+    } catch(IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in set(int,String)");
+    }
+    try{
+      SA.set(-2,"xd");
+    } catch (IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in set(int,String)");
+    }
+
+    System.out.println();
+    System.out.println("Expected : [C, B]");
+    System.out.println("SA\'s Current Status after Testing Phase 1: " + SA);
+    System.out.println("Expected : [C, B, null]");
+    System.out.println("SA\'s Current debugged Status after Testing Phase 1: " + SA.toStringDebug());
+    System.out.println();
+    System.out.println("******************************************************");
+    System.out.println();
+    System.out.println("TESTING PHASE 2 and 3 ...");
+
+    System.out.println();
+
+    System.out.println("Testing add(int.String) ... Expected : SA should be [C, D, B, E]");
+    SA.add(1,"D");
+    SA.add(3, "E");
+    System.out.println("SA\'s Current Status: " + SA);
+    System.out.println("Testing add(int,String)\'s throw ... Expected : 2 error messages");
+    try{
+      SA.add(5,"xd");
+    } catch (IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in add(int,String)");
+    }
+    try{
+      SA.add(-2,"xd");
+    } catch (IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in add(int,String)");
+    }
+
+    System.out.println();
+
+    System.out.println("Testing resize() ... Expected: SA when debugged should be [C, D, B, E, null, null, null]");
+    System.out.println(SA.toStringDebug());
+
+    System.out.println();
+
+    System.out.println("Testing contains(String) ... Expected : true");
+    System.out.println("Does SA contain \"C\" ?: " + SA.contains("C"));
+    System.out.println("Expected : false");
+    System.out.println("Does SA contain \"C\" ?: " + SA.contains("F"));
+
+    System.out.println("Testing indexOf(String) ... Expected : 3");
+    System.out.println("Where is \"E\" in SA? :" + SA.indexOf("E"));
+    System.out.println("Expected : -1");
+    System.out.println("Where is \"E\" in SA? :" + SA.indexOf("Z"));
+
+    System.out.println();
+
+    SA.add("C");
+    System.out.println("Added \"C\" to the end of the list using add(String)");
+    System.out.println("Status of SA is: " + SA);
+
+    System.out.println();
+
+    System.out.println("Testing lastIndexOf(String) ... Expected : 4");
+    System.out.println("Where is \"C\" in SA? :" + SA.lastIndexOf("C"));
+    System.out.println("Expected : -1");
+    System.out.println("Where is \"Z\" in SA? :" + SA.lastIndexOf("Z"));
+
+    System.out.println();
+
+    System.out.println("Testing remove(int) ...  Expected : Removed element is D");
+    System.out.println("The element that is removed is : " + SA.remove(1));
+    System.out.println("Expected: SA is [C, B, E, C]");
+    System.out.println("Current Status of SA: " + SA);
+    System.out.println("Testing remove(int)\'s throw ... Expected : 2 error messages");
+    try{
+      SA.remove(7);
+    } catch (IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in remove(int)");
+    }
+    try{
+      SA.remove(-2);
+    } catch (IndexOutOfBoundsException e){
+      System.out.println("Caught an Exception in remove(int)");
+    }
+
+    System.out.println();
+
+    System.out.println("Testing remove(String) ...  Expected : true");
+    System.out.println("The first element, \"C\" is removed ? : " + SA.remove("C"));
+    System.out.println("Expected: SA is [B, E, C]");
+    System.out.println("Current Status of SA: " + SA);
+    System.out.println("Expected: false");
+    System.out.println(SA.remove("ASD"));
+
+    System.out.println();
+
+    System.out.println("Testing clear() ... Expected : SA should be []");
+    SA.clear();
+    System.out.println(SA);
+
+    System.out.println();
+
+    System.out.println("END OF TESTING");
+  */}
 }
