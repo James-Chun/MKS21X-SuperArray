f643489a5dab498536083baed0da60680a1de51b
James-Chun <43070903+James-Chun@users.noreply.github.com>
2018-11-09 16:16:00 -0500

    Update SuperArray.java

--- SuperArray.java
+++ SuperArray.java
@@ -15,20 +15,15 @@ public class SuperArray {
     }
   }
   public void clear(){
-    // This resets the size to zero
     size = 0;
   }
   public int size(){
-    // This is the accessor method for size
     return size;
   }
   public boolean isEmpty(){
-    // Checks if the array is empty
     return size == 0;
   }
   public boolean add(String value){
-    // This appends the parameter value to the end of the array
-    // If the array needs to be expanded, the length of the array is doubled
     if (size == data.length){
       resize();
     }
@@ -37,14 +32,12 @@ public class SuperArray {
     return true;
   }
   public String get(int index){
-    // Returns the value at the specified index
     if (index < 0 || index >= size()){
       throw new IndexOutOfBoundsException("Index Out of Bounds");
     }
     return data[index];
   }
   public String toString(){
-    // Prints out the filled values of the array
     String ans = "[";
     for (int i = 0; i < size; i++){
       ans += get(i);
@@ -56,13 +49,12 @@ public class SuperArray {
     return ans;
   }
   public String toStringDebug(){
-    // pritns out all the values of array including the null values.
     String ans = "[";
     if (data.length == 0){
       return null;
     }
     for(int c = 0; c != data.length - 1; c++){
-      ans = ans + data[c] + ",";
+      ans = ans + data[c] + ", ";
     }
     ans = ans + data[data.length -1];
     ans = ans + "]";
@@ -70,8 +62,6 @@ public class SuperArray {
 
   }
   public String set(int index, String value){
-    // Changes the value at the specified index to the new value given in paremeter
-    // Returns the old value
     String old = data[index];
     if (index < 0 || index >= size()){
       throw new IndexOutOfBoundsException("Index Out of Bounds");
@@ -80,7 +70,6 @@ public class SuperArray {
     return old;
   }
   private void resize(){
-    // Doubles the size of the array
     String[] newArray = new String[(data.length * 2)+1];
     for (int i = 0; i < size; i++){
       newArray[i] = data[i];
@@ -112,7 +101,6 @@ public class SuperArray {
     return -1;
   }
   public void add(int index, String value){
-    // makes a new array and inserts value at index and shifting everything else to the right down by one.
     boolean inserted = false;
     String[] newArray = new String[data.length + 1];
     if (index < 0 || index > size()){
@@ -134,7 +122,6 @@ public class SuperArray {
     size ++;
   }
   public String remove(int target){
-    // removes the value at target index and returns it. Prints error if index out of range.
     boolean removed = false;
     String gone = "";
     String[] newArray = new String[data.length - 1];
@@ -157,7 +144,6 @@ public class SuperArray {
     return gone;
   }
   public boolean remove(String target){
-    // uses previous remove method to remove target string.
     if (indexOf(target) == -1){
       return false;
     }
